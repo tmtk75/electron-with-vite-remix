e8598ac2bfe877771796cca0e8cff3c6313a01b7
Tomotaka Sakuma <ktmtmks@gmail.com>
2024-11-10 19:14:45 +0900

    use import for electron module.

---
+++ src/main/index.ts
@@ -0,0 +1,36 @@
+// import { app, BrowserWindow } from "electron";
+import path, { dirname } from "node:path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const createWindow = (BrowserWindow: any) => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  win.loadURL("http://localhost:5173");
+  // win.loadFile("index.html");
+};
+
+(async () => {
+  const { app, BrowserWindow } = await import("electron");
+
+  app.whenReady().then(() => {
+    createWindow(BrowserWindow);
+
+    app.on("activate", () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow(BrowserWindow);
+      }
+    });
+  });
+
+  app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") app.quit();
+  });
+})();
---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import { name } from "./package.json";
+import camelCase from "camelcase";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+
+const IGNORE_ROLLUP_WARNING_CODE = [
+  "MISSING_GLOBAL_NAME",
+  "MISSING_NODE_BUILTINS",
+];
+
+export default defineConfig({
+  plugins: [nodePolyfills()],
+  build: {
+    lib: {
+      entry: resolve("src/main/index.ts"),
+      name: camelCase(name, { pascalCase: true }),
+      formats: ["es"],
+    },
+    rollupOptions: {
+      onwarn: (warning, defaultHandler) => {
+        if (IGNORE_ROLLUP_WARNING_CODE.includes(warning?.code)) {
+          return;
+        }
+        defaultHandler(warning);
+      },
+      external: ["electron"],
+      output: {
+        entryFileNames: "[name].mjs",
+      },
+    },
+    minify: false,
+  },
+  esbuild: {
+    platform: "node",
+  },
+});
