31982dfd27c7864bc3b0e88441ef3fd99ab0e955
Tomotaka Sakuma <ktmtmks@gmail.com>
2024-11-27 09:11:16 +0900

    support simple dev use-case
    - aim to develop renderer process.

--- src/renderer/app/routes/_index.tsx
+++ src/renderer/app/routes/_index.tsx
@@ -1,16 +1,22 @@
 import { useLoaderData } from "@remix-run/react";
 import App from "../App";
 
-// const { app } = global.electron;
+async function importElectron() {
+  if (process.env.NODE_ENV !== "production") {
+    return global.__electron__;
+  }
+  return import("electron");
+}
+
+const { app } = await importElectron();
 
 export const loader = async () => {
-  const { app } = await import("electron");
+  // const { app } = await import("electron"); // doesn't work on remix:dev (vite-dev-server)
 
   const version = app.getVersion();
   const userData = app.getPath("userData");
   console.debug("version:", version, "userData:", userData);
   return {
-    v: 1234,
     userData,
     version,
   };
@@ -21,7 +27,8 @@ export default function Index() {
   import("electron").then(({ app }) => {
     // NOTE: this runs in the main process but an error is shown in the renderer process
     //       > Uncaught (in promise) ReferenceError: __dirname is not defined
-    console.debug("version", app.getVersion());
+
+    console.debug("version", app.getVersion()); // app is undefined if vite-dev-server
   });
   console.debug("loaderData:", v);
   return (
