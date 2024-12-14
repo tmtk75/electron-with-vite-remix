1688e37ed38cb904b26f487836ecadc147c2bbcb
Tomotaka Sakuma <ktmtmks@gmail.com>
2024-12-14 11:01:37 +0900

    install shadcn/ui.sidebar-09

--- src/renderer/app/routes/_index/route.tsx
+++ src/renderer/app/routes/_index/route.tsx
@@ -3,7 +3,7 @@ import { useLoaderData } from "@remix-run/react";
 // import log from "electron-log/renderer"; // TOOD: check how to effectively use electron-log in renderer
 import { Button } from "../../components/ui/button";
 import { CommandDemo } from "./CommandDemo";
-import { LayoutWithSidebar } from "./LayoutWithSidebar";
+import Page from "../../components/page";
 
 const isDev = import.meta.env.DEV;
 console.debug("renderer: isDev:", isDev);
@@ -37,18 +37,26 @@ export default function Index() {
   const v = useLoaderData();
   console.debug("loaderData:", v);
   return (
-    <LayoutWithSidebar>
-      <div className="w-[calc(100vw-16rem)]">
-        <div className="grid grid-cols-2 gap-4 p-4">
-          <div>
+    <>
+      <Page>
+        <div className="grid grid-cols-1 gap-4 p-4">
+          <div className="mt-[300px]">
             <p className="text-sm">{JSON.stringify(v)}</p>
-            <Button size="sm" onClick={() => alert("hi")}>OK</Button>
-          </div>
-          <div>
-            <CommandDemo />
+            <Button size="sm" onClick={() => alert("Hey!")}>
+              Say Hey!
+            </Button>
+            <p className="text-sm mt-4">
+              This is an exmpale with shadcn/ui{" "}
+              <a href="https://ui.shadcn.com/blocks" className="underline">
+                buidling block sidebar-09
+              </a>
+            </p>
           </div>
         </div>
+      </Page>
+      <div className="z-10 fixed top-0 right-0 w-[500px] p-4">
+        <CommandDemo />
       </div>
-    </LayoutWithSidebar>
+    </>
   );
 }
