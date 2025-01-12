c880e58148ab16fd999a56fbe1f86b453d4d5f4b
Tomotaka Sakuma <ktmtmks@gmail.com>
2025-01-12 15:55:57 +0900

    add: enable file based routing.

--- src/renderer/app/routes.ts
+++ src/renderer/app/routes.ts
@@ -1,11 +1,17 @@
-import {
-  type RouteConfig,
-  route,
-  index,
-} from "@react-router/dev/routes";
+// https://reactrouter.com/start/framework/routing#configuring-routes
+
+// import { type RouteConfig, route, index } from "@react-router/dev/routes";
+// export default [
+//   index("./routes/_index/route.tsx"),
+//   route("welcome", "./routes/welcome.tsx"),
+//   route("page", "./routes/page.tsx"),
+//   // pattern ^           ^ module file
+// ] satisfies RouteConfig;
+
+// https://reactrouter.com/start/framework/routing#configuring-routes
+import { type RouteConfig, route } from "@react-router/dev/routes";
+import { flatRoutes } from "@react-router/fs-routes";
 
 export default [
-  index("./routes/_index.tsx"),
-  route("welcome", "./routes/welcome.tsx"),
-  // pattern ^           ^ module file
+  ...await flatRoutes(),
 ] satisfies RouteConfig;
